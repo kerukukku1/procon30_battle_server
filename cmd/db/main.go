e4b85fa58c5a1fc972f0d070ec1315c087294dcf
Mahito Fujino <kerukukku1@gmail.com>
2019-04-14 17:31:58 +0900

    設定ファイル用のstructを定義

--- cmd/db/main.go
+++ cmd/db/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type MySQLConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	DbName   string `json:"dbname"`
+}
+
 func main() {
-	db, err := sql.Open("mysql", "root:@/my_database")
+	db, err := sql.Open("mysql", "root:"+os.Getenv("GOMYSQLPASSWD")+"/my_database")
 	if err != nil {
 		panic(err.Error())
 	}
