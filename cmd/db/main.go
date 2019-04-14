7b56d5ab477e0127feb3ec6b97d52cf6e7d71da8
Mahito Fujino <kerukukku1@gmail.com>
2019-04-14 21:02:11 +0900

    change import sentence

--- cmd/db/main.go
+++ cmd/db/main.go
@@ -6,19 +6,16 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/kerukukku1/procon30_battle_server/cmd/"
 )
-
-type MySQLConfig struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	DbName   string `json:"dbname"`
-}
 
 func main() {
 	db, err := sql.Open("mysql", "root:"+os.Getenv("GOMYSQLPASSWD")+"/my_database")
 	if err != nil {
 		panic(err.Error())
 	}
+	c, err := pkg.MySQLConfigRead("config.json")
 	defer db.Close()
 	fmt.Printf("type is %T \n", db)
+	fmt.Printf("c is %T \n", c)
 }
---
+++ internal/pkg/readconfig.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type MySQLConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	DbName   string `json:"dbname"`
+}
+
+func MySQLConfigRead(filename string) (*MySQLConfig, error) {
+	c := new(MySQLConfig)
+
+	jsonStr, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return c, err
+	}
+
+	err = json.Unmarshal(jsonStr, c)
+	if err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
