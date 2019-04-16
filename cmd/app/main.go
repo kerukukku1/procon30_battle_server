4dd906cfed195123592f6082c0c3f1ec076c3881
Mahito Fujino <kerukukku1@gmail.com>
2019-04-16 21:05:29 +0900

    add router and handler and entry endpoint

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func entryHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>entry point</h1>"))
+}
 
 func main() {
-	fmt.Printf("Hello world\n")
+	// http.HandleFunc("/entry", entryHandler)
+	// http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/entry", entryHandler).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
