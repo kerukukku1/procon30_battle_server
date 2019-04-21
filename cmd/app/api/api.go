c4b4c175d08a6e00e240f42e0386c31b63ff2ebd
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-04-21 13:20:17 +0900

    add return token(JWT)

--- cmd/app/api/api.go
+++ cmd/app/api/api.go
@@ -3,11 +3,26 @@ package api
 import (
 	"net/http"
 	"fmt"
+
 	"github.com/gorilla/mux"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 func RegistHandler(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
-	fmt.Fprintf(w, "<h1>regist</h1>")
 	fmt.Println("regist", vars["userName"])
+
+	// set header
+    token := jwt.New(jwt.SigningMethodHS256)
+
+    // set claims
+    claims := token.Claims.(jwt.MapClaims)
+    claims["name"] = vars["userName"]
+
+	// make JWT
+	// TODO: signature
+    tokenString, _ := token.SignedString([]byte("signature"))
+
+	// return JWT
+	fmt.Fprintf(w, tokenString)
 }
