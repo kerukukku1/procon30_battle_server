486ab1f8b2e4b7ae29ff77a6123d3c84fe489dbf
Mahito Fujino <kerukukku1@gmail.com>
2019-04-21 16:20:24 +0900

    connection mysql and get table data

--- cmd/db/main.go
+++ cmd/db/main.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/kerukukku1/procon30_battle_server/internal/pkg"
 )
 
 func main() {
-	c, err := pkg.MySQLConfigRead("config.json")
+	c, err := pkg.MySQLConfigRead("../../config.json")
 	if err != nil {
 		panic(err.Error())
 	}
-	db, err := sql.Open("mysql", c.Username+":"+c.Password+"@/"+c.DbName)
+	db, err := sql.Open("mysql", c.Username+":"+c.Password+"@tcp(localhost:3306)/"+c.DbName)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var token string
+		if err := rows.Scan(&id, &token, &name); err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(id, token, name)
+	}
 }
