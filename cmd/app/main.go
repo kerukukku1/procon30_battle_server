3ce491c05872281380ba8e0414deda0f40c12889
Mahito Fujino <kerukukku1@gmail.com>
2019-04-21 11:29:19 +0900

    make /api/game/:roomid endpoint and empty function which save db state

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,16 +5,19 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/kerukukku1/procon30_battle_server/internal/pkg/database"
 )
 
 func entryHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>entry point</h1>"))
+	w.Write([]byte("<h1>game room</h1>"))
+	vars := mux.Vars(r)
+	database.SaveRoomState(vars["roomid"])
 }
 
 func main() {
 	// http.HandleFunc("/entry", entryHandler)
 	// http.ListenAndServe(":8080", nil)
 	router := mux.NewRouter()
-	router.HandleFunc("/entry", entryHandler).Methods("GET")
+	router.HandleFunc("/api/game/{roomid}", entryHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ internal/pkg/database/saveDB.go
@@ -0,0 +1,7 @@
+package database
+
+import "fmt"
+
+func SaveRoomState(roomName string) {
+	fmt.Print("roomName : " + roomName)
+}
